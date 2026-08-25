eb30ce6db4c55655e9db5e056d6fc20c4492e931
Dino <dino.caldironi@gmail.com>
2026-08-25 15:56:35 -0300

    proyecto base spring boot

---
+++ src/main/java/com/track3/alkywall/AlkywallApplication.java
@@ -0,0 +1,13 @@
+package com.track3.alkywall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkywallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkywallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/track3/alkywall/AlkywallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track3.alkywall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkywallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
