1dc3dd0240469939647c00378db10e428acb73a2
Dino <dino.caldironi@gmail.com>
2026-08-26 15:19:01 -0300

    feat: inicializacion de proyecto base y estructura MVC

--- src/main/java/com/track3/alkywall/AlkywallApplication.java
+++ src/main/java/com/track3/alkywall/AlkywallApplication.java
@@ -2,12 +2,15 @@ package com.track3.alkywall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AlkywallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlkywallApplication.class, args);
-	}
+    public static void main(String[] args) {
+         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        
+        SpringApplication.run(AlkywallApplication.class, args);
+    }
 
 }
