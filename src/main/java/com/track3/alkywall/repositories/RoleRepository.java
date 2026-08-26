5f3a7007da32e6aafee16b91808358fcbf9ca9e0
xaviermora <xaviermoravega11@gmail.com>
2026-08-26 17:32:09 -0300

    Agrego repositorio de Role

---
+++ src/main/java/com/track3/alkywall/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.track3.alkywall.repositories;
+
+import com.track3.alkywall.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
